975a08d36a33314db12768bae7505f999ce150f0
daniel tareke <daniel.tareke@gmail.com>
2021-07-12 11:56:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maharishi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
