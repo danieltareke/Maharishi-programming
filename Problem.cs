83f16a5582fc80f0c4167e2d038e90863c0cf4a6
daniel tareke <daniel.tareke@gmail.com>
2021-08-13 11:35:58 +0300

    added more problems and solutions

--- Problem.cs
+++ Problem.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Maharishi
+namespace Maharishi2
 {
     public class Problem
     {
@@ -15,9 +13,9 @@ namespace Maharishi
         */
         public int largestAdjacentSum(int[] a)
         {
-            int largestsum= a[0] + a[1];
+            int largestsum = a[0] + a[1];
 
-            for (int i = 0; i < a.Length-1; i++)
+            for (int i = 0; i < a.Length - 1; i++)
             {
                 if (a[i] + a[i + 1] > largestsum)
                     largestsum = a[i] + a[i + 1];
@@ -44,7 +42,7 @@ namespace Maharishi
             int isConcatenatedSum = 0;
             int sum = 0;
             int num = n;
-            
+
             while (num >= 1)
             {
                 int tempsum = 0;
@@ -78,9 +76,9 @@ namespace Maharishi
         {
             int isSequenced = 1;
             //check if there is out of range value
-            for(int i=0;i<a.Length;i++)
+            for (int i = 0; i < a.Length; i++)
             {
-                if((a[i])<m || (a[i]) > n)
+                if ((a[i]) < m || (a[i]) > n)
                 {
                     isSequenced = 0;
                     break;
@@ -88,19 +86,19 @@ namespace Maharishi
             }
             int t = m;
             //check if all exisit in the array
-            while(t<=n)
+            while (t <= n)
             {
                 bool isFound = false;
                 for (int i = 0; i < a.Length; i++)
                 {
                     if (a[i] == t)
-                    { 
+                    {
                         isFound = true;
                         break;
                     }
                 }
 
-                if(!isFound)
+                if (!isFound)
                 {
                     isSequenced = 0;
                     break;
@@ -109,9 +107,9 @@ namespace Maharishi
             }
             //check if the array is sequenced
 
-            for (int i = 0; i < a.Length-1; i++)
+            for (int i = 0; i < a.Length - 1; i++)
             {
-                if (a[i]>a[i+1])
+                if (a[i] > a[i + 1])
                 {
                     isSequenced = 0;
                     break;
@@ -121,6 +119,11 @@ namespace Maharishi
             return isSequenced;
         }
 
+        /*
+         Write a function named largestPrimeFactor that will return the largest prime factor of a number. If the
+        number is <=1 it should return 0. Recall that a prime number is a number > 1 that is divisible only by 1 and
+        itself, e.g., 13 is prime but 14 is not.
+        */
         public int largestPrimeFactor(int n)
         {
             int largestPrimeN = 0;
@@ -146,6 +149,91 @@ namespace Maharishi
             }
 
             return largestPrimeN;
+        }
+
+        /*
+         * The fundamental theorem of arithmetic states that every natural number greater than 1 can be written as a
+            unique product of prime numbers. So, for instance, 6936=2*2*2*3*17*17. Write a method named
+            encodeNumber what will encode a number n as an array that contains the prime numbers that, when
+            multipled together, will equal n. So encodeNumber(6936) would return the array {2, 2, 2, 3, 17, 17}. If the
+            number is <= 1 the function should return null;
+         */
+
+        public int[] encodeNumber(int n)
+        {
+            List<int> arrayList = new List<int>();
+
+            if (n <= 1)
+                return null;
+            else
+            {
+                int number = n;
+
+                for (int i = 2; i < number; i++)
+                {
+                    while (number % i == 0)
+                    {
+                        arrayList.Add(i);
+                        number = number / i;
+                    }
+                }
+                if (number >= 2)
+                {
+                    arrayList.Add(number);
+                }
+
+
+                return arrayList.ToArray();
+            }
+        }
+
+        /*
+         * Consider a simple pattern matching language that matches arrays of integers. A pattern is an array of
+            integers. An array matches a pattern if it contains sequences of the pattern elements in the same order as
+            they appear in the pattern. So for example, the array {1, 1, 1, 2, 2, 1, 1, 3} matches the pattern {1, 2, 1, 3}
+         */
+
+        public int matchPattern(int[] a, int len, int[] pattern, int patternLen)
+        {
+            // len is the number of elements in the array a, patternLen is the number of elements in the pattern.
+            int i = 0; // index into a
+            int k = 0; // index into pattern
+            int matches = 0; // how many times current pattern character has been matched so far
+                for (i = 0; i < len; i++)
+                {
+                    if (a[i] == pattern[k])
+                        matches++; // current pattern character was matched
+                    else if (matches == 0 || k == patternLen - 1)
+                        return 0; // if pattern[k] was never matched (matches==0) or at end of pattern (k==patternLen-1)
+                    else // advance to next pattern character 
+                    {
+                         k++; 
+                        //!!You write this code!!
+                    } // end of else
+            } // end of for
+            // return 1 if at end of array a (i==len) and also at end of pattern (k==patternLen-1)
+            if (i==len && k==patternLen-1) return 1;
+            else return 0;
+        }
+
+        /*
+         * Define the n-based integer rounding of an integer k to be the nearest multiple of n to k. If two multiples
+            of n are equidistant use the greater one. For example
+         */
+        public int[] doIntegerBasedRounding(int[] a, int n)
+        {
+            if (n > 0)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > 0)
+                    {
+                        int rem = a[i] % n;
+                        a[i] = a[i] - rem + (rem >= n / 2 ? n : 0);
+                    }
+                }
+            }
+            return a;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Maharishi
+namespace Maharishi2
 {
     class Program
     {
@@ -8,25 +8,46 @@ namespace Maharishi
         {
             Problem problem = new Problem();
             //largestAdjacentSum
-            int[] data= { 1, 1, 1, 1, 1, 2, 1, 1, 1 };
+            int[] data = { 1, 1, 1, 1, 1, 2, 1, 1, 1 };
             string response = problem.largestAdjacentSum(data).ToString();
-           // Console.WriteLine(response);
+            // Console.WriteLine(response);
 
             //checkConcatenatedSum
-            response = problem.checkConcatenatedSum(2997,3).ToString();
+            response = problem.checkConcatenatedSum(2997, 3).ToString();
             //Console.WriteLine(response);
 
             //isSequencedArray
-            int [] d= { -5, -5, -4, -4, -4, -3, -3, -2, -2, -2 };
-            response = problem.isSequencedArray(d, -5,-2).ToString();
+            int[] d = { -5, -5, -4, -4, -4, -3, -3, -2, -2, -2 };
+            response = problem.isSequencedArray(d, -5, -2).ToString();
             // Console.WriteLine(response);
 
             //largestPrimeFactor
             response = problem.largestPrimeFactor(6936).ToString();
-            Console.WriteLine(response);
+            //Console.WriteLine(response);
+
+            //encodeNumber
+            int [] res = problem.encodeNumber(1200);
+            //if (res != null)
+            //{
+            //    for (int i = 0; i < res.Length; i++)
+            //    {
+            //        Console.WriteLine(res[i] + ",");
+            //    }
+            //}
+
+            //int[] array = { 1, 2, 3 };
+            // int[] pattern = { 1, 2,3};
+            //int match = problem.matchPattern(array ,3,pattern,3);
+            // Console.WriteLine(match);
+
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] result = problem.doIntegerBasedRounding(a,2);
+            for (int i=0;i<result.Length;i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+          
+            Console.ReadKey();
         }
     }
-
-
-   
 }
